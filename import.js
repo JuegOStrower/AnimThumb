cc11b922e6ba8ffba3dfefbcbee7851c8c104bab
JuegOStrower <maxios123@hotmail.com.ar>
2017-09-21 22:31:05 -0300

    Update and rename import.js to install.user.js

--- import.js
+++
@@ -1,1 +0,0 @@
-$("<script src='https://juegostrower.github.io/Thumchange/set.js'>").appendTo(document.head);
---
+++ install.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Set animated thumbnail
+// @namespace    http://juegostrower.tk/
+// @version      1.0
+// @description  Set an animated Thumbnail in scratch projects
+// @author       JuegOStrower
+// @match        https://scratch.mit.edu/projects/*
+// @grant        none
+// ==/UserScript==
+
+$("<script src='https://juegostrower.github.io/Thumchange/set.js'>").appendTo(document.head);
