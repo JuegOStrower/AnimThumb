3306324d53d224514e7d37b304526c2a560bf20a
JuegOStrower <maxios123@hotmail.com.ar>
2017-09-21 21:06:09 -0300

    Update import.js

--- import.js
+++ import.js
@@ -1,1 +1,1 @@
-document.head.innerHTML =+ "<script src='https://juegostrower.github.io/Thumchange/set.js'>";
+document.getElementsByTagName("head")[0].innerHTML =+ "<script src='https://juegostrower.github.io/Thumchange/set.js'>";
