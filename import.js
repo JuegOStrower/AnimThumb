ee24e33397b88774360f391d5fc5e0b0592ea78f
JuegOStrower <maxios123@hotmail.com.ar>
2017-09-21 21:23:46 -0300

    Update import.js

--- import.js
+++ import.js
@@ -1,1 +1,1 @@
-document.getElementsByTagName("head")[0].innerHTML =+ "<script src='https://juegostrower.github.io/Thumchange/set.js'>";
+document.getElementsByTagName("head")[0].innerHTML += "<script src='https://juegostrower.github.io/Thumchange/set.js'>";
