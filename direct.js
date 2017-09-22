8ebb76f43444941795f665a0739d0699f61c8958
JuegOStrower <maxios123@hotmail.com.ar>
2017-09-22 16:31:10 -0300

    Update direct.js

--- direct.js
+++ direct.js
@@ -1,7 +1,7 @@
-document.getElementsByClassName("viewer")[0].innerHTML+='<input type="file" id="autothumb" onchange="upload()" style="display: none;">';
+document.getElementsByClassName("viewer")[0].innerHTML+='<input type="file" id="autothumb" onchange="upload()" style="//display: none;">';
 //$('<input type="file" id="autothumb" onchange="upload()" style="display: none;">').appendTo(document.body);
-$("#autothumb").click();
-//document.getElementById("autothumb").click();
+//$("#autothumb").click();
+document.getElementById("autothumb").click();
 function upload(){
 if (window.location.hostname === "scratch.mit.edu"){
     if (document.getElementById('report-this') === null){
