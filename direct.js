586edee3e1370be478a202505b4975fac1ee0101
JuegOStrower <maxios123@hotmail.com.ar>
2017-09-22 17:42:35 -0300

    Update direct.js

--- direct.js
+++ direct.js
@@ -1,6 +1,7 @@
 //$(document).ready(function(){
-document.getElementsByClassName("viewer")[0].innerHTML+='<input type="file" id="autothumb" style="//display: none;">';
-/*document.getElementById("autothumb").addEventListener("change", function(){
+//document.getElementsByClassName("viewer")[0].innerHTML+='<input type="file" id="autothumb" style="//display: none;">';
+$('<input type="file" id="autothumb" style="//display: none;">');
+document.getElementById("autothumb").addEventListener("change", function(){
     if (window.location.hostname === "scratch.mit.edu"){
         if (document.getElementById('report-this') === null){
             var reader = new FileReader();
@@ -23,6 +24,6 @@ document.getElementsByClassName("viewer")[0].innerHTML+='<input type="file" id="
     } else {
         alert("Please use this in a scratch project!");
     }
-});*/
+});
 document.getElementById("autothumb").click();
 //});
