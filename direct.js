3bd8573c751a073655ea5bb867c0b286e443a56e
JuegOStrower <maxios123@hotmail.com.ar>
2017-09-21 22:52:33 -0300

    Create direct.js

---
+++ direct.js
@@ -0,0 +1,22 @@
+if (window.location.hostname === "scratch.mit.edu"){
+    if (document.getElementById('report-this') === null){
+        var reader = new FileReader();
+        reader.onload = (function(e){
+            $.ajax({
+                type: "POST",
+                url: "/internalapi/project/thumbnail/" + Scratch.INIT_DATA.PROJECT.model.id + "/set/",
+                data: e.target.result,
+                contentType: "",
+                processData: false,
+                success: function(msg) {
+                    location.reload();
+                }
+            });
+        });
+        reader.readAsArrayBuffer(document.getElementById('newthumb').files[0]);
+    } else {
+        alert("This isn't one of your projects! You can only set a new title for your projects.");
+    }
+} else {
+    alert("Please use this in a scratch project!");
+}
