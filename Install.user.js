340f8c5d7ea31631ec7a3f863c0d2ed4014220d4
JuegOStrower <maxios123@hotmail.com.ar>
2018-01-16 16:51:12 -0300

    Update Install.user.js

--- Install.user.js
+++ Install.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Set animated thumbnail
 // @namespace    https://juegostrower.tk/
-// @version      2.4
+// @version      2.5
 // @description  Set an animated Thumbnail in your Scratch Projects!
 // @author       JuegOStrower
 // @include      https://scratch.mit.edu/projects/*
@@ -11,5 +11,5 @@
 
 //THIS SCRIPT WAS DEVELOPED BY @JUEGOSTROWER AND IT IS UNDER THE MIT LICENCE. IF YOU ARE GOING TO COPY IT YOU MUST GIVE CREDIT, THIS IS NOT COPYLEFT (AS SCRATCH)
 if (!document.getElementById('report-this')) {
-    $('<div onclick="var jupload=document.createElement(\'script\');jupload.src=\'https://www.juegostrower.tk/Set-Animated-Thumbnail/default.js\'; document.getElementsByTagName(\'head\')[0].appendChild(jupload);" class="action tooltip bottom" id="update-thumbnail"><span class="hovertext"><span class="arrow"></span>Set an animated Thumbnail!</span><span id="uploadButton">Upload thumbnail</span></div>').appendTo($("#stats"));
+    $('<input type="file" id="uploadPicker" style="visibility: hidden;" accept="image/*"><div onclick="var jupload=document.createElement(\'script\');jupload.src=\'https://www.juegostrower.tk/Set-Animated-Thumbnail/default.js\'; document.getElementsByTagName(\'head\')[0].appendChild(jupload);document.getElementById(\'uploadPicker\').click();" class="action tooltip bottom" id="update-thumbnail"><span class="hovertext"><span class="arrow"></span>Set an animated Thumbnail!</span><span id="uploadButton">Upload thumbnail</span></div>').appendTo($("#stats"));
 }
