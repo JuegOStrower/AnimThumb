72700d8a8d8182e18bf93d1f9c81a5438ad2a919
JuegOStrower <maxios123@hotmail.com.ar>
2018-01-17 18:16:14 -0300

    Delete bookmarklet.js

--- bookmarklet.js
+++
@@ -1,6 +0,0 @@
-try {
-    $('<input type="file" id="uploadPicker" style="visibility: hidden;" accept="image/*"><script src="https://www.juegostrower.tk/Set-Animated-Thumbnail/default.js"></script>').appendTo(document.head);
-    document.getElementById('uploadPicker').click();
-} catch (e){
-    alert("This is not one of your Scratch Projects! This tool doesn't work for other websites. :)");
-}
