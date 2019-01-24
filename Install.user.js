33f5614b70ce98444d34599a97af1749ff5d15a2
JuegOStrower <maxios123@hotmail.com.ar>
2019-01-24 13:11:46 -0300

    SUPPORT FOR SCRATCH 3.0!

--- Install.user.js
+++ Install.user.js
@@ -1,20 +1,48 @@
 // ==UserScript==
-// @name         Set animated thumbnail
+// @name         Set animated thumbnail | @JuegOStrower
 // @namespace    https://juegostrower.tk/
-// @version      3.2
+// @version      4.0
 // @description  Set an animated Thumbnail in your Scratch Projects!
 // @author       JuegOStrower
 // @include      https://scratch.mit.edu/projects/*
 // @exclude      https://scratch.mit.edu/projects/*/*/
 // @grant        none
+// @icon         https://www.juegostrower.tk/favicon.png
+// @icon64       https://www.juegostrower.tk/favicon.png
+// @supportURL   https://github.com/JuegOStrower/AnimThumb/issues
 // ==/UserScript==
 
 //THIS SCRIPT WAS DEVELOPED BY @JUEGOSTROWER AND IT IS UNDER THE MIT LICENCE. IF YOU ARE GOING TO COPY IT YOU MUST GIVE CREDIT, THIS IS NOT COPYLEFT (AS SCRATCH)
-if ((!document.getElementById('report-this')) && typeof Scratch.INIT_DATA.LOGGED_IN_USER.model !== 'undefined') {
-    try {
-        $('<div onclick="var jupload=document.createElement(\'script\');jupload.src=\'https://www.juegostrower.tk/AnimThumb/default.js\'; document.getElementsByTagName(\'head\')[0].appendChild(jupload);var jupload2 = document.createElement(\'input\');jupload2.id=\'uploadPicker\';jupload2.type=\'file\';jupload2.style = \'visibility: hidden;display:none;\';jupload2.accept = \'image/*\';document.body.appendChild(jupload2);document.getElementById(\'uploadPicker\').click();" class="action tooltip bottom" id="update-thumbnail"><span class="hovertext"><span class="arrow"></span>Set an animated Thumbnail!</span><span id="uploadButton">Upload thumbnail</span></div>').appendTo($("#stats"));
-    } catch(e) {
-        alert("Animated Thumbnail found an unxpected error. Please tell to the developer (@JuegOStrower) in her profile comments to fix the problem.");
-        console.log(e);
-    }
+try {
+	var juegostrowerAnimthumbCheck = setInterval(function() {
+		if (document.getElementsByClassName("flex-row action-buttons")[0]) {
+			document.getElementsByClassName("flex-row action-buttons")[0].innerHTML+='<button id="juegostrower-animthumb" class="button action-button" title="By @JuegOStrower">Animated Thumbnail</button>';
+			document.getElementById("juegostrower-animthumb").addEventListener("click", juegostrowerAnimthumbStart);
+			clearInterval(juegostrowerAnimthumbCheck);
+		}
+	}, 100);
+} catch(e) {
+	alert("Animated Thumbnail found an unxpected error adding the button. Please comment on the @JuegOStrower (the developer, aka me) profile to help to fix the problem.");
+	console.log(e);
+}
+function juegostrowerAnimthumbStart(){
+	if(!document.getElementById("juegostrower-animthumb-picker")){
+		try {
+			var juegostrowerAnimthumbUpload=document.createElement("input");
+			juegostrowerAnimthumbUpload.id="juegostrower-animthumb-picker";
+			juegostrowerAnimthumbUpload.type="file";
+			juegostrowerAnimthumbUpload.accept="image/*";
+            juegostrowerAnimthumbUpload.style="visibility: hidden; position: absolute; top: 0; left: -5000px;";
+            document.getElementsByTagName("head")[0].appendChild(juegostrowerAnimthumbUpload);
+            juegostrowerAnimthumbUpload.click();
+			var juegostrowerAnimthumbScript=document.createElement("script");
+			juegostrowerAnimthumbScript.src="https://www.juegostrower.tk/AnimThumb/default.js";
+			document.getElementsByTagName("head")[0].appendChild(juegostrowerAnimthumbScript);
+        } catch(e){
+			alert("Animated Thumbnail found an unxpected error importing the script. Please comment on the @JuegOStrower (the developer, aka me) profile to help to fix the problem.");
+            console.log(e);
+		}
+	} else {
+		if(document.getElementById("juegostrower-animthumb-upload").style.display!=="none")document.getElementById("juegostrower-animthumb-picker").click();
+	}
 }
