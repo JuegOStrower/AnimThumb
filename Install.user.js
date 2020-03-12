94763f80a5581912c7367b3095ff95c776eaf11c
Maxi Os <maxios123@hotmail.com.ar>
2020-03-12 18:10:19 -0300

    Domain change

--- Install.user.js
+++ Install.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Set animated thumbnail | @JuegOStrower
-// @namespace    https://juegostrower.tk/
+// @namespace    https://juegostrower.github.io
 // @version      4.0
 // @description  Set an animated Thumbnail in your Scratch Projects!
 // @author       JuegOStrower
 // @include      https://scratch.mit.edu/projects/*
 // @exclude      https://scratch.mit.edu/projects/*/*/
 // @grant        none
-// @icon         https://www.juegostrower.tk/favicon.png
-// @icon64       https://www.juegostrower.tk/favicon.png
+// @icon         https://www.juegostrower.github.io/favicon.png
+// @icon64       https://www.juegostrower.github.io/favicon.png
 // @supportURL   https://github.com/JuegOStrower/AnimThumb/issues
 // ==/UserScript==
 
@@ -37,7 +37,7 @@ function juegostrowerAnimthumbStart(){
         }
         if(!document.getElementById("juegostrower-animthumb-banner")){
             var juegostrowerAnimthumbScript=document.createElement("script");
-            juegostrowerAnimthumbScript.src="https://www.juegostrower.tk/AnimThumb/default.js";
+            juegostrowerAnimthumbScript.src="https://www.juegostrower.github.io/AnimThumb/default.js";
             document.getElementsByTagName("head")[0].appendChild(juegostrowerAnimthumbScript);
         }
         try {
